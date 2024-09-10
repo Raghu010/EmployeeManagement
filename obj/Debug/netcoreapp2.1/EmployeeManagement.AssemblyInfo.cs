66a74e2c08e6f4d7f211bf99e6f259b59b4d7893
Raghu <raghuraghav929@gmail.com>
2024-09-10 09:03:58 +0530

    EmployeeManagement Complete Project

--- obj/Debug/netcoreapp2.1/EmployeeManagement.AssemblyInfo.cs
+++ obj/Debug/netcoreapp2.1/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9499edae9ec35c85af3667d3fc3c410027d2f10f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
